9c30eee65d7037d56130a49fd85d83b60b31e6ca
lsjuandiego <jlopez@almeraim.com>
2024-01-22 15:16:22 -0500

    tipado de themoviedb

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,13 +2,14 @@ import { useNavigation } from '@react-navigation/native'
 import React, { useEffect } from 'react'
 import { Button, Text, View } from 'react-native'
 import movieDB from '../api/movieDB'
+import { MovieDBNowPlaying } from '../interfaces/movieInterface'
 
 export const HomeScreen = () => {
 
     useEffect(() => {
-        movieDB.get('/now_playing').then(
+        movieDB.get<MovieDBNowPlaying>('/now_playing').then(
             resp => {
-                console.log(resp.data);
+                console.log(resp.data.results[0].title);
             }
         );
     }, []);
